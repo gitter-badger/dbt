9e9b570c27dfdf7db3742082ee4b3e9df2a97df6
Drew Banin <dsb59@drexel.edu>
2016-03-17 14:24:00 -0400

    resolve dependencies in compiler

--- dbt/task/compile.py
+++ dbt/task/compile.py
@@ -3,18 +3,21 @@ import os
 import fnmatch
 import jinja2
 
+from .. import project
 
 class CompileTask:
     def __init__(self, args, project):
         self.args = args
         self.project = project
 
-    def __src_index(self):
+        self.deps_path = os.path.join(os.path.expanduser('~'), '.dbt/deps')
+
+    def __src_index(self, project):
         """returns: {'model': ['pardot/model.sql', 'segment/model.sql']}
         """
         indexed_files = {}
 
-        for source_path in self.project['source-paths']:
+        for source_path in project['source-paths']:
             for root, dirs, files in os.walk(source_path):
                 for filename in files:
                     if fnmatch.fnmatch(filename, "*.sql"):
@@ -24,8 +27,8 @@ class CompileTask:
 
         return indexed_files
 
-    def __write(self, path, payload):
-        target_path = os.path.join(self.project['target-path'], path)
+    def __write(self, project, path, payload):
+        target_path = os.path.join(project['target-path'], path)
 
         if not os.path.exists(os.path.dirname(target_path)):
             os.makedirs(os.path.dirname(target_path))
@@ -35,13 +38,22 @@ class CompileTask:
         with open(target_path, 'w') as f:
             f.write(payload)
 
-    def __compile(self, src_index):
+    def __compile(self, project, src_index):
         for src_path, files in src_index.iteritems():
             jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=src_path))
             for f in files:
                 template = jinja.get_template(f)
-                self.__write(f, template.render(self.project.context()))
+                self.__write(project, f, template.render(self.project.context()))
+
+    def __get_deps(self):
+        dep_names = [dep['name'] for dep in self.project.cfg.get('deps', [])]
+
+        for dep_name in dep_names:
+            dep_config = os.path.join(self.deps_path, dep_name, 'dbt_project.yml')
+            dep_project = project.read_project(dep_project)
+            print dep_project
 
     def run(self):
-        src_index = self.__src_index()
-        self.__compile(src_index)
+        #src_index = self.__src_index(self.project)
+        #self.__compile(self.project, src_index)
+        self.__get_deps()
