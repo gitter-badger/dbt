bded7ee437b8804fd5e32b308eeecc0959e71c6b
Drew Banin <dsb59@drexel.edu>
2016-03-23 11:33:37 -0400

    don't write files to users home dir

--- setup.py
+++ setup.py
@@ -5,28 +5,10 @@ import os.path
 package_name = "dbt"
 package_version = "0.1.0-SNAPSHOT"
 
-config_dir = os.path.join(os.path.expanduser('~'), '.dbt')
-
-sample_profiles_file = "sample.profiles.yml"
-sample_profiles_contents = ""
-with open(sample_profiles_file) as fh:
-    sample_profiles_contents = fh.read()
-
-data_files = []
-if not os.path.exists(config_dir):
-    os.path.makedirs(config_dir)
-
-config_file = os.path.join(config_dir, 'profiles.yml')
-if not os.path.exists(config_file):
-
-    with open(config_file, 'w') as fh:
-        fh.write(sample_profiles_contents)
-
 setup(
   name=package_name,
   version=package_version,
   packages=find_packages(),
-  data_files=data_files,
   scripts=[
     'scripts/dbt',
   ],
